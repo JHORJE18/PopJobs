2e8701979af849d9f4f3fbc8cf78b74c2fc62dad
Jorge Lopez Gil <wiijlg@hotmail.com>
2018-02-05 17:31:39 +0100

    Clase Usuario

---
+++ app/src/main/java/com/jhorje18/popjobs/Objetos/Usuario.java
@@ -0,0 +1,22 @@
+package com.jhorje18.popjobs.Objetos;
+
+/**
+ * Created by JHORJE on 5/2/18.
+ */
+
+public class Usuario {
+
+    //Variables
+    private String nombre;
+
+    public Usuario() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
--- app/src/main/java/com/jhorje18/popjobs/Principal.java
+++ app/src/main/java/com/jhorje18/popjobs/Principal.java
@@ -12,6 +12,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.jhorje18.popjobs.Objetos.Usuario;
 
 public class Principal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -31,6 +36,14 @@ public class Principal extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("clav");
+
+        Usuario obj = new Usuario();
+        obj.setNombre("JOrge");
+        myRef.setValue(obj);
     }
 
     @Override
