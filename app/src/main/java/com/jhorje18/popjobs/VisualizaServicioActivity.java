904bc251f00d658ba1f9ab1fc61f8b32f17d62c8
abbox <abbox>
2018-02-06 17:41:43 +0100

    cambios Ventana visualizar servicio

--- app/src/main/java/com/jhorje18/popjobs/VisualizaServicioActivity.java
+++ app/src/main/java/com/jhorje18/popjobs/VisualizaServicioActivity.java
@@ -1,9 +1,11 @@
 package com.jhorje18.popjobs;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
@@ -20,6 +22,21 @@ public class VisualizaServicioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_visualiza_servicio);
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id==R.id.menuEditar)
+        {
+            Intent intent = new Intent(VisualizaServicioActivity.this,NuevoServicioActivity.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -28,4 +45,8 @@ public class VisualizaServicioActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.visualizar_servicio,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+
+
+
 }
