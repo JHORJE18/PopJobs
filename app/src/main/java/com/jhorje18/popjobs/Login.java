5e2160b3dacd27a6b829a2322f2c712b312455d6
Jose.Vte <jvtMari@gmail.com>
2018-02-07 15:53:58 +0100

    Inicio Sesion
    Casi completo el inciio de sesion con google.

--- app/src/main/java/com/jhorje18/popjobs/Login.java
+++ app/src/main/java/com/jhorje18/popjobs/Login.java
@@ -9,12 +9,20 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,6 +36,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
     //Finals
     private final static int GOOGLE_SINGIN_INTENT = 4;
+    private final static int ACTIVITY_REGISTRO = 3;
 
     //GoogleApi
     GoogleApiClient googleApiClient;
@@ -35,7 +44,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
     //Firebase
     DatabaseReference referenciaUsuarios;
-
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +53,12 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
         //Iniciaciones Firebase
         referenciaUsuarios = FirebaseDatabase.getInstance().getReference("usuarios");
+        firebaseAuth = FirebaseAuth.getInstance();
+
 
         //Iniciaciones GoogleApi
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -56,7 +68,8 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 .build();
 
 
-        signInButtonGoogle = (SignInButton) findViewById(R.id.btn_SingIn_Google) ;
+        signInButtonGoogle = findViewById(R.id.btn_SingIn_Google) ;
+        signInButtonGoogle.setColorScheme(SignInButton.COLOR_DARK);
         signInButtonGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -77,34 +90,57 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
             case GOOGLE_SINGIN_INTENT:
                 GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
                 Test_Resultado_SingInGoogle(result);
-             break;
+                break;
+            case ACTIVITY_REGISTRO:
+                switch (resultCode){
+                    case RESULT_OK:
+                        break;
+                    case RESULT_CANCELED:
+                        break;
+                }
+                break;
+
         }
     }
 
     private void Test_Resultado_SingInGoogle(GoogleSignInResult result) {
         if(result.isSuccess()){
-            Test_New_User(result.getSignInAccount().getId());
+            Test_New_User(result);
         }else{
             Toast.makeText(getApplicationContext(), String.valueOf(result.getStatus().getStatusCode()), Toast.LENGTH_LONG).show();
             Toast.makeText(getApplicationContext(), "Login con Google Fallido", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void Abrir_Activity_Principal() {
-        Intent i = new Intent(this, Principal.class);
-        startActivity(i);
-        finish();
+    private void Abrir_Activity_Principal(final GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent i = new Intent(getApplicationContext(), Principal.class);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Error Logeando con las credenciales de google en firebase", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    private void Abrir_Activity_Registrarse() {
+      //  Intent i = new Intent(this, )
     }
 
-    private void Test_New_User(String idUser) {
+    private void Test_New_User(final GoogleSignInResult result) {
+        String idUser = result.getSignInAccount().getId();
         Query q = referenciaUsuarios.child(idUser);
         q.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if(dataSnapshot.exists()){
-                    Abrir_Activity_Principal();
+                    Abrir_Activity_Principal(result.getSignInAccount());
                 }else{
-                    //Abrir_Activity_Registrarse
+                    Abrir_Activity_Registrarse(); //En el activity recoger el la cuenta y generar credencial.
                 }
             }
 
--- app/src/main/java/com/jhorje18/popjobs/SplashActivity.java
+++ app/src/main/java/com/jhorje18/popjobs/SplashActivity.java
@@ -2,39 +2,77 @@ package com.jhorje18.popjobs;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SplashActivity extends AppCompatActivity {
+public class  SplashActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    //Finals
     private final int DURACION_SPLASH = 1000;
+
+    //Google APi
+    private GoogleApiClient googleApiClient;
+    private GoogleSignInOptions gso;
+
+    //Firebase
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        //Iniciaciones Firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //Iniciaciones Google Api
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                //Obtener detalles del Auth
-                FirebaseUser userAuth = FirebaseAuth.getInstance().getCurrentUser();
-
-                //Si la Auth ya se Inicio va al Activity Principal
-                if (userAuth != null){
-                    startActivity(new Intent(SplashActivity.this, Principal.class));
-                    finish();
-                } else {
-                    Toast.makeText(SplashActivity.this, "No has Iniciado Sesión", Toast.LENGTH_SHORT).show();
-                    //TODO Redirigir a la pantalla Inicio de Sesión
-                    startActivity(new Intent(SplashActivity.this, Login.class));
-                    finish();
+                if(firebaseAuth.getCurrentUser() != null){
+                    Abrir_Activity_Principal();
+                }else{
+                    Abrir_Acitivty_Login();
                 }
-
             }
         }, DURACION_SPLASH);
+    }
+
+    private void Abrir_Acitivty_Login() {
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+        finish();
+    }
+
+
+    private void Abrir_Activity_Principal(){
+        Intent i = new Intent(this, Principal.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(getApplicationContext(),"Error Realizando la conexion de GoogleApiClient",Toast.LENGTH_LONG).show();
     }
 }
