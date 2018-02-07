6dca4dd01d2c10b48aa0b277d372da2c666773d3
Damir Galimov <drenatovich@gmail.com>
2018-02-07 17:28:26 +0100

    Registro Usuarios creando usuario y añadiendo a firebase
    En proceso...

--- app/src/main/java/com/jhorje18/popjobs/Objetos/Usuario.java
+++ app/src/main/java/com/jhorje18/popjobs/Objetos/Usuario.java
@@ -7,16 +7,81 @@ package com.jhorje18.popjobs.Objetos;
 public class Usuario {
 
     //Variables
-    private String nombre;
+    private String uid;
+    private String nombreApellidos;
+    private String fechaNacimiento;
+    private String direccion;
+    private String correo;
+    private String imagenKey;
+    private int telefono;
 
     public Usuario() {
+
     }
 
-    public String getNombre() {
-        return nombre;
+    public Usuario(String uid, String nombreApellidos, String fechaNacimiento, String direccion, String correo, String imagenKey, int telefono) {
+        this.uid = uid;
+        this.nombreApellidos = nombreApellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.imagenKey = imagenKey;
+        this.telefono = telefono;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombreApellidos() {
+        return nombreApellidos;
+    }
+
+    public void setNombreApellidos(String nombreApellidos) {
+        this.nombreApellidos = nombreApellidos;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getImagenKey() {
+        return imagenKey;
+    }
+
+    public void setImagenKey(String imagenKey) {
+        this.imagenKey = imagenKey;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
     }
 }
